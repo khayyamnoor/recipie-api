6e05bb65da43f169c95b8a246ffd670d7f02a7ed
Khayyam Noor <khayyamnoor6@gmail.com>
2025-03-10 20:16:38 +0500

    custom admin setting

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,53 @@
+# app/core/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_active', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    #you can read only
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+    search_fields = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
+
+
+
+
+
+
